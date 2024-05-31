3e95de37b3686ba6451c5df3ef0d0db4afc669af
leejugy <lee09260@naver.com>
2024-06-01 00:03:11 +0900

    joy stick!

---
+++ Core/Src/main.c
@@ -0,0 +1,264 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "dma.h"
+#include "usart.h"
+#include "usb_device.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+typedef enum{
+	UART1 = 0,
+	CDC
+}TX_Type;
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define PC13_Toggle HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_13)
+#define STICK_Y_PIN GPIO_PIN_1
+#define STICK_X_PIN GPIO_PIN_0
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint8_t uart_rx_data;
+bool uart_rx_flag = false;
+
+uint8_t *cdc_rx_data;
+bool cdc_rx_flag = false;
+
+uint32_t ADC_VAL[3];
+
+uint8_t tx_buffer[32];
+extern uint8_t CDC_Transmit_FS(uint8_t* Buf, uint16_t Len);
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if(huart->Instance==huart1.Instance){
+		uart_rx_flag = true;
+		HAL_UART_Receive_DMA(&huart1, &uart_rx_data, 1);
+	}
+  UNUSED(huart);
+}
+
+void TX(TX_Type tx_type, char *fmt,...){
+	va_list arg;
+	va_start(arg,fmt);
+	vsnprintf((char *)&tx_buffer[0],32,fmt,arg);
+
+	if(tx_type==UART1){
+		HAL_UART_Transmit(&huart1,tx_buffer,32,10);
+	}
+	else if(tx_type==CDC){
+		CDC_Transmit_FS(tx_buffer,32);
+	}
+	va_end(arg);
+	memset(tx_buffer,0,32);
+}
+
+void read_joystick_dma(){
+	TX(UART1,"X_VAL:%d\n",ADC_VAL[0]);
+	TX(UART1,"Y_VAL:%d\n",ADC_VAL[1]);
+	TX(UART1,"SW_VAL:%d\n",ADC_VAL[2]);
+	//x
+	if(ADC_VAL[0]>=3000){
+		TX(CDC,"<-\n");
+	}
+	else if(ADC_VAL[0]<=500){
+		TX(CDC,"->\n");
+	}
+
+	//y
+	if(ADC_VAL[1]>=3000){
+		TX(CDC,"^\n");
+	}
+	else if(ADC_VAL[1]<=500){
+		TX(CDC,"v\n");
+	}
+
+	else if(ADC_VAL[2]<=500){
+		TX(CDC,"Switch click!\n");
+	}
+}
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART1_UART_Init();
+  MX_USB_DEVICE_Init();
+  MX_ADC1_Init();
+  /* USER CODE BEGIN 2 */
+  uint32_t pretime=HAL_GetTick();
+
+
+  UART_Start_Receive_DMA(&huart1, &uart_rx_data, 1);
+  HAL_ADC_Start_DMA(&hadc1,&ADC_VAL[0], 3);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+  	if(HAL_GetTick()-pretime>100){
+  		PC13_Toggle;
+  		pretime = HAL_GetTick();
+  		read_joystick_dma();
+  	}
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
